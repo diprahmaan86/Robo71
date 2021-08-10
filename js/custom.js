23b66e98597bce5fa3eb4484fa1f07797a7eed23
MD. Daud-Ar-Rahman <84992914+diprahmaan86@users.noreply.github.com>
2021-08-11 02:53:58 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    // Program Slider
+
+    $('.pro_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        speed: 1500,
+    });
+
+
+});
